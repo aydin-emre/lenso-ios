4e9863a8998b07e55f195cf41b56692ec447c513
Emre AYDIN <ea@ea.tc>
2025-09-13 14:16:37 +0300

    Project is created..

---
+++ Lenso/Lenso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lenso
+//
+//  Created by Emre on 13.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
